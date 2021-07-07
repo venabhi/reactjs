369e4e681ceeed0e9f3baeb2c3ed32a9145854d0
vabhi <cvabhi94@gmail.com>
2021-07-07 13:08:40 -0500

    season display

--- seasonDisplay.js
+++ seasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+// determining season
+const GetSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  console.log(props.lat);
+  const Season = GetSeason(props.lat, new Date().getMonth());
+  console.log(Season);
 
   return <div>season display</div>;
 };
